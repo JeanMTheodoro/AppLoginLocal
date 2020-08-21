4658a4ab008bc35369ff923a4b739e134d6ee6d0
JeanMTheodoro <jean.theodorosilva@outlook.com>
2020-08-21 15:26:04 -0300

    Preferencias

--- app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
+++ app/src/main/java/br/edu/dmos5/loginlocal/MainActivity.java
@@ -3,6 +3,7 @@ package br.edu.dmos5.loginlocal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,6 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
     private EditText usuarioEditText;
     private EditText senhaEditText;
@@ -35,6 +39,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         lembrarCheckBox = findViewById(R.id.checkbox_lembrar);
         novoUsuarioTextView = findViewById(R.id.textview_novo);
         logarButton.setOnClickListener(this);
+        novoUsuarioTextView.setOnClickListener(this);
+
+        mSharedPreferences = this.getPreferences(MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
     }
 
 
@@ -57,6 +65,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onResume() {
 
         Log.i(getString(R.string.tag), "Classe: " + getClass().getSimpleName() + "| Método : onResume()");
+        verificarPreferencias();
         super.onResume();
     }
 
@@ -95,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 return;
             }
 
+            salvaPreferencias();
             abrirBoasVindas();
             return;
         }
@@ -109,4 +119,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         in.putExtras(args);
         startActivity(in);
     }
+
+
+    private void salvaPreferencias() {
+
+        if (lembrarCheckBox.isChecked()) {
+            mEditor.putString(getString(R.string.key_usuario), usuario);
+            mEditor.commit();
+            mEditor.putString(getString(R.string.key_senha), senha);
+            mEditor.commit();
+            mEditor.putBoolean(getString(R.string.key_lembrar), true);
+            mEditor.commit();
+        } else {
+            mEditor.putString(getString(R.string.key_usuario), "");
+            mEditor.commit();
+            mEditor.putString(getString(R.string.key_senha), "");
+            mEditor.commit();
+            mEditor.putBoolean(getString(R.string.key_lembrar), false);
+            mEditor.commit();
+        }
+
+    }
+    private void verificarPreferencias() {
+        usuario = mSharedPreferences.getString(getString(R.string.key_usuario), "");
+        senha = mSharedPreferences.getString(getString(R.string.key_senha), "");
+        boolean lembrar = mSharedPreferences.getBoolean(getString(R.string.key_lembrar), false);
+        if (lembrar) {
+            usuarioEditText.setText(usuario);
+            senhaEditText.setText(senha);
+            lembrarCheckBox.setChecked(true);
+        }
+
+    }
+
 }
